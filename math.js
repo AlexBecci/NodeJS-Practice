9c61713a54605b01ecfb901d7ad4d43511dafe96
AlexBecci <beccialex2002@gmail.com>
2023-08-30 16:51:50 -0300

    created functions in math.js

--- math.js
+++ math.js
@@ -1,12 +1,16 @@
-function add(x1,x2) {
-    return x1 + x2;
+function add(x1, x2) {
+  return x1 + x2;
 }
-function substract(x1,x2) {
-    return x1 - x2;
+function substract(x1, x2) {
+  return x1 - x2;
 }
-function multiply(x1,x2) {
-    return x1 * x2;
+function multiply(x1, x2) {
+  return x1 * x2;
 }
-function add(x1,x2) {
-    return x1 + x2;
+function divide(x1, x2) {
+  if (x2 == 0) {
+    return console.log("No se puede dividir ya que el dividendo es ", x2);
+  } else {
+    return x1 / x2;
+  }
 }
