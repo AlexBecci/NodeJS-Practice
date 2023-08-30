2d9b3f5b354e71146c8f5c7e8c748d2a637f2668
AlexBecci <beccialex2002@gmail.com>
2023-08-30 17:31:03 -0300

    readFile and writeFile

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const fs = require("fs");
 
-fs.writeFile('./text.txt', 'linea uno......',function (err) {
+fs.writeFile("./text.txt", "linea uno......", function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Archivo creado");
+});
 
-    if(err){
-        console.log(err)
-    }
-    console.log('Archivo creado');
-})
+console.log("ultima linea");
 
+fs.readFile("./text.txt", function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+  if (data) {
+    console.log(data.toString());
+  }
+});
 
-console.log('ultima linea')
-
-fs.readFile()
